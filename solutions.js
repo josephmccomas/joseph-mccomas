bfefe39e3ede2b3cb2150d7116c0982f66c9db2a
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-31 14:48:29 -0600

    Practice Test

--- solutions.js
+++ solutions.js
@@ -1,5 +1,21 @@
 "use strict";
-
+const people = [ {
+    name: "Bob",
+    age: 42
+}, {
+    name: "Sue",
+    age: 27
+}, {
+    name: "Lou",
+    age: 33
+}, {
+    name: "Zanzibar",
+    age: 18
+}, {
+    name: "Gladys",
+    age: 39
+}
+];
 /**
  * Write your solutions here
  *
@@ -22,12 +38,69 @@ console.log(isBoolean(false));
 // #2
 //Write a function called hasBoolean that returns true if the array parameter contains at least one element that is a boolean. Otherwise, it returns false.
 
-function hasBoolean(array) {
-    for (let i = 0; i < array.length; i++) {
-        return typeof array[i] === "boolean";
+function hasBoolean(input){
+    for (let i = 0; i < input.length; i++) {
+        if(typeof input[i] === "boolean"){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 console.log(hasBoolean([1, 2, 3, 4])); // outputs false
 console.log(hasBoolean([1, "false", 3, 4])); // outputs false
 console.log(hasBoolean([1, 2, true, 4])); // outputs true
+
+// #3
+//Write a function called minAge that takes an array of people as a parameter and returns the smallest age in the array.
+
+function minAge(people){
+    let lowestAge = 100;
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].age < lowestAge){
+            lowestAge = people[i].age;
+        }
+    }
+    return lowestAge;
+}
+
+console.log(minAge(people)); // outputs 18
+
+// #4
+//Write a function called longestName that takes an array of people as a parameter and returns the longest name in the array.
+
+function longestName(input) {
+    let longName = "";
+    for (let i = 0; i < input.length; i++) {
+        if (input[i].name.length > longName.length){
+            longName = input[i].name;
+        }
+    }
+    return longName;
+}
+
+console.log(longestName(people)); // outputs Zanzibar
+
+// #5
+//Write a function called createPerson that takes a name parameter and an age parameter and returns an object containing the given name and age as properties.
+
+function createPerson(name, age){
+    let user = {
+        "name": name,
+        "age": age
+    }
+    return user;
+}
+
+const jimbob = createPerson("Jimbob", 22);
+console.log(jimbob);
+/* outputs
+{
+name: "Jimbob",
+age: 22
+}
+*/
+
+//#6
+// When the button with an id of bold-btn is clicked, that button's font weight should change to bold.
