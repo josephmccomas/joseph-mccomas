8d0e968d28e17f365e60e1da6802c1e7087ddb0a
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-01-31 13:39:31 -0600

    Practice Test

--- solutions.js
+++ solutions.js
@@ -7,3 +7,27 @@
  * immediately invoked function expression (iife), you should _not_ do that
  * here. If you do, the automated tests will not be able to see your functions.
  */
+
+// #1
+//Write a function called isBoolean that returns true if the parameter has a data type of boolean. Otherwise, it returns false.
+
+function isBoolean(input) {
+    return input === true;
+}
+
+console.log(isBoolean(42)); // outputs false
+console.log(isBoolean("true")); // outputs false
+console.log(isBoolean(false));
+
+// #2
+//Write a function called hasBoolean that returns true if the array parameter contains at least one element that is a boolean. Otherwise, it returns false.
+
+function hasBoolean(array) {
+    for (let i = 0; i < array.length; i++) {
+        return typeof array[i] === "boolean";
+    }
+}
+
+console.log(hasBoolean([1, 2, 3, 4])); // outputs false
+console.log(hasBoolean([1, "false", 3, 4])); // outputs false
+console.log(hasBoolean([1, 2, true, 4])); // outputs true
